e1f426355fc2b6a709106804af1eb1989aa06d4d
James Shvarts <jshvarts@gmail.com>
2022-12-26 20:36:10 -0500

    Add basic UI for user list in compose

--- app/src/main/java/com/jshvarts/viewmodelflowdemo/ComposeActivity.kt
+++ app/src/main/java/com/jshvarts/viewmodelflowdemo/ComposeActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.jshvarts.viewmodelflowdemo.ui.theme.ViewModelFlowDemoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -38,19 +43,38 @@ fun MainScreen(
   modifier: Modifier = Modifier
 ) {
   val userList by viewModel.userList.collectAsState()
-  render(userList)
-}
-
-private fun render(uiState: UiState) {
-  when (uiState) {
+  when (userList) {
     UiState.Loading -> {
       println("jls loading")
+      Box(
+        modifier = modifier
+          .fillMaxSize()
+      ) {
+        CircularProgressIndicator()
+      }
     }
     is UiState.Success -> {
-      println("jls success ${uiState.data}")
+      LazyColumn {
+        items((userList as UiState.Success).data) { item ->
+          Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = modifier
+              .fillParentMaxWidth()
+          ) {
+            Text(item.name)
+            Text("${item.age}")
+          }
+        }
+      }
     }
     is UiState.Error -> {
-      println("jls error $uiState")
+      Box(
+        modifier = modifier
+          .fillMaxSize()
+      ) {
+        (userList as UiState.Error).throwable
+      }
     }
   }
 }
+
